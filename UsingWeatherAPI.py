230d438682d64745cc76986f49ac9135d66d1fe5
micher94 <55001134+micher94@users.noreply.github.com>
2020-04-06 18:53:54 -0400

    Update UsingWeatherAPI.py
    Changing so it's not asking for user input

--- UsingWeatherAPI.py
+++ UsingWeatherAPI.py
@@ -6,36 +6,49 @@ import requests
 
 # Levereged information from this youtube video on how to use the Open weather API: https://www.youtube.com/watch?v=lcWfSn6-m_8
 
-Coord = input('What latitude and longitude do you want to look at(format Lat, long)')
-Timeframe = input('What time frame do you want to look at? if you want to look at next available forecast, please type "now"')
+def main(Coord,Timeframe):
 
-# This commented line is how to do it using the Open Weather API. The Key is not included
-# api_access = 'http://api.openweathermap.org/data/2.5/weather?appid=&q=Boston'
 
-# This next line is how to access with the Weather API
-api_access_bare = 'https://api.weather.gov/points/'
-# Combine the bare api link with the coordinates input by the user
-api_access = api_access_bare + Coord
-# print(api_access)
-json_data = requests.get(api_access).json()
-# print(json_data)
+	#Coord = input('What latitude and longitude do you want to look at(format Lat, long)')
+	#Timeframe = input('What time frame do you want to look at? if you want to look at next available forecast, please type "now"')
 
-# from the initial json data, find the link to the forecast data
-forecast_access = json_data['properties']['forecast']
-# print(forecast_access)
+	# This commented line is how to do it using the Open Weather API. The Key is not included
+	# api_access = 'http://api.openweathermap.org/data/2.5/weather?appid=&q=Boston'
 
-# request the forecast data
-new_json_data = requests.get(forecast_access).json()
-# print(new_json_data)
+	# This next line is how to access with the Weather API
+	api_access_bare = 'https://api.weather.gov/points/'
+	# Combine the bare api link with the coordinates input by the user
+	api_access = api_access_bare + Coord
+	# print(api_access)
+	json_data = requests.get(api_access).json()
+	# print(json_data)
 
-# go through the available forcast data based on the timeframe name and pull the one most similar to the user.
-if Timeframe=='now':
-	for periods in new_json_data['properties']['periods']:
-	# print(periods['name'])
-		if periods['number']==1:
-			print(periods['temperature'], periods['temperatureUnit'])
-else:
-	for periods in new_json_data['properties']['periods']:
+	# from the initial json data, find the link to the forecast data
+	forecast_access = json_data['properties']['forecast']
+	# print(forecast_access)
+
+	# request the forecast data
+	new_json_data = requests.get(forecast_access).json()
+	# print(new_json_data)
+
+	# go through the available forcast data based on the timeframe name and pull the one most similar to the user.
+	if Timeframe=='now':
+		for periods in new_json_data['properties']['periods']:
 		# print(periods['name'])
-		if periods['name']==Timeframe:
-			print(periods['temperature'], periods['temperatureUnit'])
+			if periods['number']==1:
+				temp = periods['temperature']
+				units = periods['temperatureUnit']
+				print(periods['temperature'], periods['temperatureUnit'])
+	else:
+		for periods in new_json_data['properties']['periods']:
+			# print(periods['name'])
+			if periods['name']==Timeframe:
+				temp = periods['temperature']
+				units = periods['temperatureUnit']
+				print(periods['temperature'], periods['temperatureUnit'])
+	return print(temp,units)
+
+
+
+if __name__ == '__main__':
+    main()
