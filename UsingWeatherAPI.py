5feaa5bcc895595077c8e4ed70ac3be46867e407
micher94 <55001134+micher94@users.noreply.github.com>
2020-04-09 23:14:16 -0400

    Add files via upload
    added try except for error handling

--- UsingWeatherAPI.py
+++ UsingWeatherAPI.py
@@ -41,48 +41,55 @@ def weatherdata(iata):
 
 
 
+	try:
+		Coord = Airports[iata]
+		Timeframe = 'now'
 
-	Coord = Airports[iata]
-	Timeframe = 'now'
+		# print(Coord)
 
-	# print(Coord)
+		# This commented line is how to do it using the Open Weather API. The Key is not included
+		# api_access = 'http://api.openweathermap.org/data/2.5/weather?appid=&q=Boston'
 
-	# This commented line is how to do it using the Open Weather API. The Key is not included
-	# api_access = 'http://api.openweathermap.org/data/2.5/weather?appid=&q=Boston'
+		# This next line is how to access with the Weather API
+		api_access_bare = 'https://api.weather.gov/points/'
+		# Combine the bare api link with the coordinates input by the user
+		api_access = api_access_bare + Coord
+		# print(api_access)
+		
+		
+		json_data = requests.get(api_access).json()
+		# print(json_data)
 
-	# This next line is how to access with the Weather API
-	api_access_bare = 'https://api.weather.gov/points/'
-	# Combine the bare api link with the coordinates input by the user
-	api_access = api_access_bare + Coord
-	# print(api_access)
-	json_data = requests.get(api_access).json()
-	# print(json_data)
+		# from the initial json data, find the link to the forecast data
+		forecast_access = json_data['properties']['forecast']
+		# print(forecast_access)
 
-	# from the initial json data, find the link to the forecast data
-	forecast_access = json_data['properties']['forecast']
-	# print(forecast_access)
+		# request the forecast data
+		new_json_data = requests.get(forecast_access).json()
+		# print(new_json_data)
 
-	# request the forecast data
-	new_json_data = requests.get(forecast_access).json()
-	# print(new_json_data)
-
-	# go through the available forcast data based on the timeframe name and pull the one most similar to the user.
-	if Timeframe=='now':
-		for periods in new_json_data['properties']['periods']:
-		# print(periods['name'])
-			if periods['number']==1:
-				temp = periods['temperature']
-				units = periods['temperatureUnit']
-				# print(periods['temperature'], periods['temperatureUnit'])
-	else:
-		for periods in new_json_data['properties']['periods']:
+		# go through the available forcast data based on the timeframe name and pull the one most similar to the user.
+		if Timeframe=='now':
+			for periods in new_json_data['properties']['periods']:
 			# print(periods['name'])
-			if periods['name']==Timeframe:
-				temp = periods['temperature']
-				units = periods['temperatureUnit']
-				print(periods['temperature'], periods['temperatureUnit'])
-	# return print(temp,units)
-	return print('complete')
+				if periods['number']==1:
+					temp = periods['temperature']
+					units = periods['temperatureUnit']
+					# print(periods['temperature'], periods['temperatureUnit'])
+		else:
+			for periods in new_json_data['properties']['periods']:
+				# print(periods['name'])
+				if periods['name']==Timeframe:
+					temp = periods['temperature']
+					units = periods['temperatureUnit']
+					print(periods['temperature'], periods['temperatureUnit'])
+		print(temp,units)
+		return True
+	except KeyError as error:
+		print('I am an exception')
+		return False
+	else:
+		return False
 
 
 if __name__ == '__main__':
