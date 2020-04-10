389f558fc8174dbef1ad61887755c90d0949c710
micher94 <55001134+micher94@users.noreply.github.com>
2020-04-09 23:15:31 -0400

    Update testing.py
    testing error handling

--- testing.py
+++ testing.py
@@ -2,6 +2,9 @@ from UsingWeatherAPI import weatherdata
 import pytest
 
 def test_values():
-	weatherdata('IAD') == "Complete"
-	weatherdata('BWI') == "Complete"
-	weatherdata('DUB') == "Complete"
+	weatherdata('IAD') == True
+	weatherdata('BWI') == True
+	weatherdata('DUB') == True
+	weatherdata('ID') == False
+	weatherdata('###') == False
+	weatherdata('123') == False
