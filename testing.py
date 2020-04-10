d6a712edd826235c0b79a4d1efb73cefa8bff73e
micher94 <55001134+micher94@users.noreply.github.com>
2020-04-09 23:00:52 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,10 @@
 from UsingWeatherAPI import weatherdata
+import pytest
 
-def main():
-	weatherdata('IAD')
+def test_values():
+	weatherdata('IAD') == "Complete"
+	weatherdata('BWI') == "Complete"
+	weatherdata('DUB') == "Complete"
 
 
 if __name__ == '__main__':
